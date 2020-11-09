94e10924902671019413196f1b87633e6b117f4a
bkj <ben@canfield.io>
2020-11-09 22:04:48 +0000

    testing

--- test.py
+++ test.py
@@ -1,21 +1,27 @@
 import sys
 sys.path.append('.')
 sys.path.append('build')
-import gunrock_sssp
+from gunrock_sssp import gunrock_sssp
 
 import numpy as np
 from scipy.io import mmread
 
+np.set_printoptions(linewidth=240)
+
 # Load graph
 csr = mmread('chesapeake.mtx').tocsr()
+
+n_vertices = csr.shape[0]
+n_edges    = csr.nnz
 
 # Allocate host memory for output
 distances    = np.zeros(csr.shape[0]).astype(np.float32)
 predecessors = np.zeros(csr.shape[0]).astype(np.int32)
 
-# Run
-gunrock_sssp.gunrock_sssp(csr.shape[0], csr.nnz, csr.indptr, csr.indices, csr.data, 0, distances, predecessors)
+# Run + print output
+for single_source in range(csr.shape[0]):
+  gunrock_sssp(n_vertices, n_edges, csr.indptr, csr.indices, csr.data, single_source, distances, predecessors)
+  print(distances)
 
-# Print output
-print('Distances:', distances)
-print('Predecessors:', predecessors)
+# !! This copies data to/from GPU too many times ... how can we work around?
+#    Should allow user to create graph object in Python, but having issues w/ types
