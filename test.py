84fee3d13d1b028d526b0b3da46b96947d0330fc
bkj <ben@canfield.io>
2020-11-20 17:21:35 +0000

    optimizing

--- test.py
+++ test.py
@@ -19,7 +19,8 @@ from scipy.io import mmread
 np.set_printoptions(linewidth=240)
 
 # Load graph
-csr = mmread('chesapeake.mtx').tocsr()
+# csr = mmread('chesapeake.mtx').tocsr()
+csr = mmread('cit-Patents-sub.mtx').tocsr()
 
 n_vertices = csr.shape[0]
 n_edges    = csr.nnz
@@ -36,6 +37,9 @@ predecessors = torch.zeros(csr.shape[0]).int().cuda()
 # Create graph
 G = pyg.Graph(n_vertices, n_edges, indptr, indices, data)
 
-for single_source in range(n_vertices):
+t = time()
+for single_source in trange(1000):
   _ = pyg.sssp(G, single_source, distances, predecessors)
-  print(distances.cpu().numpy())
+  # print(distances.cpu().numpy())
+
+print(time() - t)
